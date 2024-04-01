cf2ba5e20b882884e4b8311d0efc32e70f9c8108
Ba9A-tata <yogadzakkii@gmail.com>
2024-04-01 16:06:35 +0700

    merubah cin menjadi getline di struct 3 pada bagian int main()

--- activitystruct_0089/activitystruct_0089.cpp
+++ activitystruct_0089/activitystruct_0089.cpp
@@ -65,3 +65,44 @@ int main() {
 	cout << "\n \t Desa : " << mhs.alamat.desa;
 	cout << "\n Umur : " << mhs.umur;
 }
+
+#include <iostream>
+using namespace std;
+
+struct DetailAlamat {
+	string desa;
+	string kota;
+};
+
+struct Mahasiswa {
+	string nim;
+	string nama;
+	DetailAlamat alamat;
+	string umur;
+};
+
+int main() {
+	Mahasiswa mhs;
+	cout << "Nomor Mahasiswa : ";
+	getline(cin,mhs.nim);
+	cout << "Nama Mahasiswa : ";
+	getline(cin,mhs.nama);
+
+	cout << "Aalamat Mahasiswa : " << endl;
+	cout << "t Nama Desa : ";
+	cin >> mhs.alamat.desa;
+	cout << "t Nama Kota : ";
+	cin >> mhs.alamat.kota;
+
+
+	cout << " Umur Mahasiswa : ";
+	cin >> mhs.umur;
+
+	cout << endl;
+	cout << "\n NIM : " << mhs.nim;
+	cout << "\n Nama : " << mhs.nama;
+	cout << "\n Alamat : " << mhs.alamat;
+	cout << "\n \t Desa : " << mhs.alamat.desa;
+	cout << "\n Umur : " << mhs.umur;
+}
+
